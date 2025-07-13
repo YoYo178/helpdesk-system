afd0b3b3fba2f448605846c58017258fe6206a8d
YoYo178 <95132723+YoYo178@users.noreply.github.com>
2025-07-13 19:06:46 +0530

    Update NotFound.tsx

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -1,8 +1,13 @@
+import { Link } from "react-router-dom"
+
 export const NotFound = () => {
     return (
         <>
             <h1>404</h1>
             <p>The page you are looking for cannot be found!</p>
+            <p>
+                Did you mean to visit <Link to='/login'>/login</Link>?
+            </p>
         </>
     )
 }
